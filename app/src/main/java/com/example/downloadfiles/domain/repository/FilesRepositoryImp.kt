b086adabe4bda50e6a6ec693fe09f3ffc4263870
MohamedGElsharkawy <Mohammedgammal96@gmail.com>
2022-01-21 13:35:10 +0200

    Implement gateway request files

--- app/src/main/java/com/example/downloadfiles/domain/repository/FilesRepositoryImp.kt
+++ app/src/main/java/com/example/downloadfiles/domain/repository/FilesRepositoryImp.kt
@@ -3,9 +3,15 @@ package com.example.downloadfiles.domain.repository
 import com.example.downloadfiles.domain.gateway.FilesApi
 import com.example.downloadfiles.entity.remotefiles.FilesRemoteResponse
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class FilesRepositoryImp(server:FilesApi):FilesRepository {
-    override suspend fun getFiles(): Flow<List<FilesRemoteResponse>> {
-        TODO("Not yet implemented")
+class FilesRepositoryImp(private val server: FilesApi) : FilesRepository {
+
+    /**
+     * Request files from gateway, then emit to [Flow]
+     */
+    override suspend fun getFiles(): Flow<List<FilesRemoteResponse>> = flow {
+        val response = server.getFiles()
+        emit(response)
     }
 }
